8f615f36164300a0fa4ee2ed96f514a54bf63824
Arnold Schulte <arnoldschulte@gmail.com>
2022-12-07 07:57:30 +0100

    AOC-05: init Day 05

---
+++ src/main/java/nl/schulte/advent/day05/Day05.java
@@ -0,0 +1,13 @@
+package nl.schulte.advent.day05;
+
+import nl.schulte.advent.util.TextParser;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Day05 {
+
+    public static void main(String[] args) throws IOException {
+        final List<String> textLines = TextParser.getTextLines("input/day05.txt");
+    }
+}
